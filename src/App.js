c98ffd54db234dc1fbab728d45f4ff501c918bcf
Paul Smal <pavel.smal@gmail.com>
2019-02-07 20:16:55 +0100

    simple react app to test usrhub

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import UsrHub from 'usrhub';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.sendState = this.sendState.bind(this);
+    this.setState = this.setState.bind(this);
+    this.hub = UsrHub({
+      secret_key: "THINKPAD",
+      usr: {name: "Paul"},
+      hub: {name: "hub:1"},
+      func: this.setState
+    })
+    this.state = this.hub.test()
+  }
+
+  sendState(event) {
+    this.hub.call("shout", {
+      x: event.clientX,
+      y: event.clientY
+    })
+  }
+
+  setState(payload) {
+    console.log(payload)
+    this.setState({
+      x: payload.clientX,
+      y: payload.clientY
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div style={{ height: '100%' }} onMouseMove={this.sendState}>
+            <h1>Move the mouse around!</h1>
+            <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
+          </div>
         </header>
       </div>
     );
