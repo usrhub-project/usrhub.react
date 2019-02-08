1517d3f961ba72ff300924ffd6c2ca1f960f8643
Paul Smal <pavel.smal@gmail.com>
2019-02-08 11:32:22 +0100

    react hooks test

--- src/App.js
+++ src/App.js
@@ -1,46 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import UsrHub from 'usrhub';
 import './App.css';
 
+function Example() {
+  const [count, setCount] = useState(0);
+  
+  const func = (state) => {
+    setCount(state.count)
+  }
+
+  const hub = UsrHub({secret_key: "TP", usr: {name: "Paul"}, hub: {name: "hub:1"}, func});
+  
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => hub.call("inc", {"count": count})}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.sendState = this.sendState.bind(this);
-    this.setState = this.setState.bind(this);
-    this.hub = UsrHub({
-      secret_key: "THINKPAD",
-      usr: {name: "Paul"},
-      hub: {name: "hub:1"},
-      func: this.setState
-    })
-    this.state = this.hub.test()
-  }
-
-  sendState(event) {
-    this.hub.call("shout", {
-      x: event.clientX,
-      y: event.clientY
-    })
-  }
-
-  setState(payload) {
-    console.log(payload)
-    this.setState({
-      x: payload.clientX,
-      y: payload.clientY
-    });
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <div style={{ height: '100%' }} onMouseMove={this.sendState}>
-            <h1>Move the mouse around!</h1>
-            <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
-          </div>
+          <Example />
         </header>
       </div>
     );
